45909134b661c366e338a618f76fd39702569802
chessnerdbird <dblackw2@gmail.com>
2018-03-12 02:36:37 -0400

    Passed User testing

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+	it "is invalid without a username" do
+		user = User.new(username: nil)
+		expect(user).not_to be_valid
+	end
+
+	it "is invalid without a email" do 
+		email = User.new(email: nil)
+		expect(email).not_to be_valid
+	end
+
+	it "is valid with required attributes" do
+		user = User.new(username: "David", email: "david@example.com")
+		expect(user).to be_valid
+	end
+end
