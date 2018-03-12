41ced9f63f230d2ae27b28cbeb0353ce7363548d
chessnerdbird <dblackw2@gmail.com>
2018-03-12 16:54:49 -0400

    Blog failed validation - blog must have a title

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it "is invalid without a title"
+  it "is invalid without a title" do
+  	blog = Post.new(title: nil)
+  	expect(blog).not_to be_valid
+  end
 
   it "is invalid without a article"
 end
