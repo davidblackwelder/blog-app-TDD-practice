6a607d76170cea83159a21793ce02edeb27a13b4
chessnerdbird <dblackw2@gmail.com>
2018-03-12 16:58:42 -0400

    Validation invalid without a article fails with nil

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  
   it "is invalid without a title" do
   	blog = Post.new(title: nil)
   	expect(blog).not_to be_valid
   end
 
-  it "is invalid without a article"
+  it "is invalid without a article" do 
+  	article = Post.new(article: nil)
+  	expect(blog).not_to be_valid
+  end
 end
