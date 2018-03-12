6946aa91384723699f6b5f31148facb27371580f
chessnerdbird <dblackw2@gmail.com>
2018-03-12 17:03:26 -0400

    Blog passes is valid with required attributes(title, article)

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,12 +3,17 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   
   it "is invalid without a title" do
-  	blog = Post.new(title: nil)
-  	expect(blog).not_to be_valid
+  	title = Post.new(title: nil)
+  	expect(title).not_to be_valid
   end
 
   it "is invalid without a article" do 
   	article = Post.new(article: nil)
   	expect(article).not_to be_valid
+  end
+
+  it "is valid with required attributes" do 
+  	blog = Post.new(title: "First Post", article: "Example text in the article")
+  	expect(blog).to be_valid
   end
 end
